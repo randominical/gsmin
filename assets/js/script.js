a8f7d5af13c44907d2e05d1115fe33dc0ce6df0e
randominical <aa.agafonova@gmail.com>
2021-01-13 22:58:40 +0300

    swiper основной слайдер кастомизация стрелок; слайдер готов (без адаптива)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,13 @@
-/*let mySwiper = new Swiper('.carousel', {
-    wrapperClass: "carousel__wrapper",
-    slideClass: "carousel__item",
-
-    slidesPerView: 1,
-    pagination: {
-      el: '.carousel__pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.carousel__button-next',
-      prevEl: '.carousel__button-prev',
-    },
-  
-  })*/
-  var mySwiper = new Swiper ('.carousel__container', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: false,
-    slidesPerView: 1,
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true,
-    },
-
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  })
+let mySwiper = new Swiper ('.carousel__container', {
+  direction: 'horizontal',
+  loop: false,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+})
