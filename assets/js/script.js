ce041bd2fcb9121c91c9d137038ac0a6897be9e3
randominical <aa.agafonova@gmail.com>
2021-01-26 22:42:30 +0300

    Revert "hits, new swiper правки по разрешению"
    This reverts commit 4d401b60da1478569c644d90432a76024d0575c1.

--- assets/js/script.js
+++ assets/js/script.js
@@ -73,6 +73,7 @@ let newSlider = undefined;
     if ( (screenWidth < (768)) && (newSlider == undefined)) {
       newSlider = new Swiper('.new__container', {
         direction: 'horizontal',
+        spaceBetween: 30,
         loop: true,
         navigation: {
           nextEl: '.new__btn_n',
@@ -96,6 +97,7 @@ let newSlider = undefined;
     if ( (screenWidth < (768)) && (hitsSlider == undefined)) {
       hitsSlider = new Swiper('.hits__container', {
         direction: 'horizontal',
+        spaceBetween: 30,
         loop: true,
         navigation: {
           nextEl: '.hits__btn_n',
